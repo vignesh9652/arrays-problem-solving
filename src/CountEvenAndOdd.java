e5e3dd8e7c4c7100aa0f7dba5c654d03bcf7dd11
vignesh9652 <panchavativignesh@gmail.com>
2026-06-17 12:11:06 +0530

    maximum and minimum of array

---
+++ src/CountEvenAndOdd.java
@@ -0,0 +1,19 @@
+public class CountEvenAndOdd {
+    public static void main(String[] args) {
+      int[] nums = {1,3,2,4,5,78};
+        countEvenAndOdd(nums);
+    }
+    public static void countEvenAndOdd(int[] nums){
+          int evenCount = 0;
+          int oddCount = 0;
+          for(int num : nums){
+              if (num %2== 0){
+                  evenCount ++;
+              }else {
+                  oddCount ++;
+              }
+          }
+        System.out.println("Even Count: " + evenCount);
+        System.out.println("Odd Count: " + oddCount);
+    }
+}
---
+++ src/FindMaxAndMIn.java
@@ -0,0 +1,20 @@
+public class FindMaxAndMIn {
+    public static void main(String[] args) {
+     int[] nums = {12,3,4,2,7};
+     findMaxAndMin(nums);
+    }
+    public static void findMaxAndMin(int[] nums){
+        int max = nums[0];
+        int min = nums[0];
+        for(int num : nums){
+            if(num > max){
+                max = num;
+            }
+            if (num < min){
+                min = num;
+            }
+        }
+        System.out.println("Min: " + min);
+        System.out.println("Max: " + max);
+    }
+}
