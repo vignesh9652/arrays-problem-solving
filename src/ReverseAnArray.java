fc016e3e174e8da8d9fa44ef420736eb95415c45
vignesh9652 <panchavativignesh@gmail.com>
2026-06-17 23:58:42 +0530

    Reverse of an array

---
+++ src/ReverseAnArray.java
@@ -0,0 +1,10 @@
+public class ReverseAnArray {
+    public static void main(String[] args) {
+        int[] nums = {1,2,3,4,5,6};
+        reverseAnArray(nums);
+    }
+    public static void reverseAnArray(int[] nums){
+        for(int i = nums.length -1; i>=0; i--)
+          System.out.print(nums[i] + "");
+  }
+}
