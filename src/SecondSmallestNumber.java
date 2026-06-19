b45bf1ce788735f3a5bc7084a4279917843c6cd8
vignesh9652 <panchavativignesh@gmail.com>
2026-06-19 10:49:12 +0530

    SecondSmallestNumber

---
+++ src/SecondSmallestNumber.java
@@ -0,0 +1,21 @@
+public class SecondSmallestNumber {
+    public static void main(String[] args) {
+        int[] nums = {10,3,2,4,7,8,1};
+        System.out.println( secondSmallestNumber(nums));
+    }
+    public static int secondSmallestNumber(int[] nums){
+        int min = nums[0];
+        int smallestMin = nums[0];
+        for (int num : nums){
+            if(num < min){
+                min =num;
+            }
+        }
+        for (int num : nums){
+            if(num < smallestMin){
+                smallestMin = num;
+            }
+        }
+        return smallestMin;
+    }
+}
